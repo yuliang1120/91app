cb99d688361783017aaea0a5bc79bd3071c3105a
yuliang1120 <43166939+yuliang1120@users.noreply.github.com>
2018-09-11 16:35:25 +0800

    Add files via upload

---
+++ week1 (2).py
@@ -0,0 +1,20 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+
+
+# In[9]:
+
+
+df = pd.read_csv("C:\\Users\\user\\Desktop\\Pecu Python\\NTU_大數據與商業分析Datasets\\Orders.csv")
+
+
+# In[10]:
+
+
+df.head()
+
